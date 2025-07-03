13f08aa2765b4457be74a2e6a936922b8d702478
Marvin <marvinbarretto@gmail.com>
2025-07-03 14:47:13 +0100

    bump env

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -21,7 +21,6 @@ export const environment: Environment = {
     chatId: 'YOUR_TELEGRAM_CHAT_ID'
   },
   featureFlags: {
-    patchwork: false,
     landlord: false,
     theme: true,
     search: false,
