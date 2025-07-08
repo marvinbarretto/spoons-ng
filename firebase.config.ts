6beafe911d275decb9932c7d9a6e932e793d3eb3
Marvin <marvinbarretto@gmail.com>
2025-07-08 15:27:35 +0100

    chore: Update Firestore rules for carpet storage

--- firebase.config.ts
+++ firebase.config.ts
@@ -3,11 +3,14 @@ import { environment } from "./src/environments/environment";
 import { provideFirebaseApp } from "@angular/fire/app";
 import { provideFirestore } from "@angular/fire/firestore";
 import { provideAuth } from "@angular/fire/auth";
+import { provideStorage } from "@angular/fire/storage";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 export const firebaseProviders = [
   provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
   provideFirestore(() => getFirestore()),
   provideAuth(() => getAuth()),
+  provideStorage(() => getStorage()),
 ]
